4bb1cca79f32bbe70b1c27268b578ed4c6b87555
SylviaRemington <sylviaremington@gmail.com>
2025-08-16 12:46:56 -0700

    adding useEffect to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 // src/App.jsx
 import { useEffect, useState } from "react";
+import { index} from "./services/petService";
 import "./App.css";
 
 
 function App () {
+
+  useEffect(() => {
+
+  }, []);
+
+
+
   return <h1>Hello world!</h1>;
 };
 
