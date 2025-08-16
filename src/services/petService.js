f7aefee2b0015aa4bd3efb931b610f1acceb2def
SylviaRemington <sylviaremington@gmail.com>
2025-08-16 11:04:04 -0700

    lots of code added yesterday for petService.js & gitignore json packages

--- src/services/petService.js
+++ src/services/petService.js
@@ -1,1 +1,32 @@
 // touch src/services/petService.js
+// src/services/petService.js
+// ___________________________________________
+// IMPORTS
+
+import axios from "axios";
+
+
+
+
+
+
+
+
+// ___________________________________________
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+// THIS CREATES THE BASE URL.
+//  Environment variables are loaded when the app is first run, 
+// and Vite does not track changes to the .env file. 
+// If you alter your .env file, you must restart your development server 
+// for the changes to take effect.
+
+// ABOVE IS THE SAME AS WRITING THE FOLLOWING:
+// const BASE_URL = `http://localhost:3000/pets`;
+
+const index = async () => {
+    const res = await axios.get(BASE_URL);
+    return res.json();
+}
+
+
